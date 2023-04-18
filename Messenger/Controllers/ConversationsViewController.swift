c00c78ebdb79fee896af15c45036e52117511587
iamkimbert <iamkimbert@gmail.com>
2023-04-18 11:47:06 -0400

    Wrap up getting started

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  Messenger
 //
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
