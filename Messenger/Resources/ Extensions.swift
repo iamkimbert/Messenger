c8b1cd3d9e32c101cf44e60c6c164e57d33cb1a0
iamkimbert <iamkimbert@gmail.com>
2023-04-28 15:53:52 -0400

    Handle auth session, add database

---
+++ Messenger/Resources/ Extensions.swift
@@ -0,0 +1,8 @@
+//
+//   Extensions.swift
+//  Messenger
+//
+//  Created by Kimberly Townsend on 4/18/23.
+//
+
+import Foundation
---
+++ Messenger/Resources/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  Messenger
+//
+//  Created by Kimberly Townsend on 4/28/23.
+//
+
+import Foundation
