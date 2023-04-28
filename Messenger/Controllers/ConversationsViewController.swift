a26b7a3c7ad5d01036e0c961d82916cf1edf99af
iamkimbert <iamkimbert@gmail.com>
2023-04-28 16:05:29 -0400

    Handle auth session, add database

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -1,19 +1,47 @@
- //
+//
 //  ViewController.swift
 //  Messenger
 //
 //  Created by Kimberly Townsend on 4/17/23.
 //
+//
 
 import UIKit
+import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
+//        DatabaseManager.shared.test()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+    }
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false )
+            
+        }
+    }
+    
+    //validateAuth()
+    //        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        //
+        //        if !isLoggedIn{
+        //            let vc = LoginViewController()
+        //            let nav = UINavigationController(rootViewController: vc)
+        //            nav.modalPresentationStyle = .fullScreen
+        //            present(nav, animated: false )
+    
+    
 }
 
--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager{
+    
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+}
+
+// MARK: - Acccount Mgmt
+extension DatabaseManager {
+    public func userExists(with email: String,
+                           completion: @escaping((Bool) -> Void)){
+        
+        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+                
+            }
+            
+            completion(true)
+        })
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: ChatAppUser) {
+        database.child(user.emailAddress).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName
+        ])
+    }
+}
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    // not good practice to do a let password
+    //    let profilePictureUrl: String
+}
